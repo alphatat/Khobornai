12bd33449cef4220b83d6bfdd3b4188308778b3e
alphatat <fordrive2242@gmail.com>
2025-05-28 09:41:07 +0600

    Master Commit 1

--- app/src/main/java/com/alif/khobornai/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/alif/khobornai/ui/screens/HomeScreen.kt
@@ -1,5 +1,15 @@
 package com.alif.khobornai.ui.screens
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.alif.khobornai.data.model.Newspaper
+
 @Composable
 fun HomeScreen(newspapers: List<Newspaper>) {
     LazyColumn {
