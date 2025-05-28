ddf5e59e03de0148a064251593d478c26943d7db
alphatat <fordrive2242@gmail.com>
2025-05-28 08:28:51 +0600

    Master Commit 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.alif.khobornai"
-    compileSdk = 35
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.alif.khobornai"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
